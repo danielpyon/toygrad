e37341d751bfbc752e09f3e8896e670d1f9f70df
Daniel Pyon <pyondohyun@gmail.com>
2022-01-04 17:24:01 -0800

    grads are zero for some reason

--- toygrad/Network.js
+++ toygrad/Network.js
@@ -1,21 +1,40 @@
+let Scalar = require("./Scalar.js");
+
 class Network {
     
 }
 
 class ReLUNeuron {
-    constructor(W, b) {
-        this.W = W; // [n x 1] array of Scalar
-        this.b = b;
+    constructor(n) {
+        // initialize weights randomly
+        this.W = new Array(n);
+        
+        // Standard Normal variate using Box-Muller transform.
+        function randn_bm() {
+            var u = 0, v = 0;
+            while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+            while(v === 0) v = Math.random();
+            return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+        }
+
+        for (let i = 0; i < this.W.length; i++)
+            this.W[i] = new Scalar(randn_bm());
+
+        this.b = new Scalar(0.0);
     }
     forward(x) {
         // given x: [n x 1]
         // return ReLU(W@x + b)
         
+        for (let i = 0; i < x.length; i++)
+            if (!(x[i] instanceof Scalar))
+                x[i] = new Scalar(x[i]);
+
         this.x = x; // store x
         
         let product = new Scalar(0.0);
-        for (let i = 0; i < W.length; i++) {
-            product = product.add(W[i].mul(x[i]));
+        for (let i = 0; i < this.W.length; i++) {
+            product = product.add(this.W[i].mul(x[i]));
         }
 
         let out = product.add(this.b).relu();
@@ -26,10 +45,18 @@ class ReLUNeuron {
     backward(dout) {
         // given dL/dout
         // backpropagate the gradient to inputs
-        this.out.backprop(dout);
+        this.out.backward();
     }
     
 }
 
-module.export.Network = Network;
-module export.ReLUNeuron = ReLUNeuron;
+
+let NN = new ReLUNeuron(4);
+NN.forward([1, 2, 3, 4]);
+NN.backward(1.0);
+
+console.log(NN.x[0].grad);
+console.log(NN.W[0].grad);
+
+module.exports.Network = Network;
+module.exports.ReLUNeuron = ReLUNeuron;
